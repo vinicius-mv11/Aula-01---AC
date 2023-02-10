ea8d8739e033cafcea39702748836ecc990158dc
vinicius-mv11 <88984568+vinicius-mv11@users.noreply.github.com>
2023-02-10 23:19:56 +0000

    formatando

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
+
 http.createServer(function(req, res) {
   res.writeHead(200, { 'content-type': 'text/plain' });
-  res.end('Servidor criado em Node.js!\n');
+  res.end('Servidor criado em node.js!\n');
 }).listen(3000, () => console.log('server started'))
