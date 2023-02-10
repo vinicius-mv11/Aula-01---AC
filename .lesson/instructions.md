abd5ad28640bb9aa76cba76fc4dd115e433630db
vinicius-mv11 <88984568+vinicius-mv11@users.noreply.github.com>
2023-02-10 23:03:45 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,4 @@
+# Instruções  
+
+- Modifique o arquivo index.js e crie um servidor node.js.
+- A página deverá apresentar essa característica: https://aula-01.dswa5.repl.co/
---
+++ index.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  res.end('Servidor criado em Node.js!\n');
+}).listen(3000, () => console.log('server started'))
